def3e52b77543773011f22e23d3f98a44a2d1e35
David Chen <darkbaby123@gmail.com>
2017-11-15 09:48:21 +0800

    change mix.exs

--- mix.exs
+++ mix.exs
@@ -31,7 +31,8 @@ defmodule DemoDeploy.Mixfile do
 
   defp version do
     {cmt, _} = System.cmd("git", ["rev-parse", "HEAD"])
-    pre_release = String.slice(cmt, 0..6)
+    pre_release = String.slice(cmt, 0..5)
+    IO.puts ">>> cmt: #{cmt}"
 
     vsn = "0.1.0"
     "#{vsn}-#{pre_release}"
